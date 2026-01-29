0622198d4e22a623b6833543b43529dae6010fba
Ibrahim <ishaheen910@gmail.com>
2026-01-29 16:17:59 +0200

    init

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AddExpense from "../screens/AddExpense/AddExpense.screen";
+import ExpenseDetails from "../screens/ExpenseDetails/ExpenseDetails.screen";
+import HomeScreen from "../screens/Home/Home.screen";
+
+export type RootStackParamList = {
+  Home: undefined;
+  AddExpense: undefined;
+  ExpenseDetails: { id: string };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="AddExpense" component={AddExpense} />
+      <Stack.Screen name="ExpenseDetails" component={ExpenseDetails} />
+    </Stack.Navigator>
+  );
+}
